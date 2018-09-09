59c9168895c14cc1513c9568107949d8e356c465
ygout <yohann.goutaret@gmail.com>
2018-09-09 18:38:11 +0200

    config

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,14 @@
-// tslint:disable-next-line:no-console
-console.log('Hello World ! ');
+import * as fs from 'fs';
+import * as fsPath from 'fs-path';
+import { dofusService } from './services/dofus.service';
+import * as cm from './cli-view/cmd';
+import { asciiHelper } from './helpers/asciHelper';
+
+
+// starting up scrapper
+async function init(): Promise<void> {
+
+    asciiHelper.asciiArt();
+
+}
+
